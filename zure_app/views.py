df61e0a6e7ec30bed57a0898bff9b3729bec1f46
FWashington1999 <F.Washington1999@outlook.com>
2025-05-13 00:06:57 -0300

    add auth cadastro (test only version)

--- zure_app/views.py
+++ zure_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Produto
+from django.http import HttpResponse
+from django.contrib.auth.models import User
 
 def home(request):
     destaques = Produto.objects.filter(destaques=True)[:4]
@@ -18,7 +20,23 @@ def destaque4(request):
     return render(request, 'zure_app/destaque4.html')
 
 def cadastro(request):
-    return render(request, 'zure_app/account-cadastro.html')
+    if request.method == 'GET':  
+        return render(request, 'zure_app/account-cadastro.html')
+    else:
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        telefone = request.POST.get('phone')
+        senha = request.POST.get('password')
+        senha2 = request.POST.get('confirm-password')
+
+        email_exite = User.objects.filter(email=email).exists()
+
+        if email_exite:
+            return HttpResponse("Email já cadastrado")
+        
+        user = User.objects.create_user(username=name, email=email, password=senha)
+        user.save()
+        return HttpResponse('Cadastro realizado com sucesso!')
 
 def login(request):
     return render(request, 'zure_app/account-login.html')
