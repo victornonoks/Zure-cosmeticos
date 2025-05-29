318bae02438ad232b3fba19b1819ec0a321640cc
victornonoks <victornksdev@gmail.com>
2025-05-29 08:55:28 -0300

    Atlterações no models

--- zure_app/admin.py
+++ zure_app/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
+from django.utils.html import format_html 
 from .models import Produto
 
 #Registra o modelo Produto no admin do Django
 admin.site.register(Produto)
+
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'descricao_formatada', 'preco', 'destaques')
+
+    def descricao_formatada(self, obj):
+        # converte cada quebra de linha em <br>
+        return format_html(obj.descricao.replace('\n', '<br>'))
+    descricao_formatada.short_description = 'Descrição'
--- zure_app/models.py
+++ zure_app/models.py
@@ -3,9 +3,13 @@ from django.db import models
 class Produto(models.Model):
     nome = models.CharField(max_length=100)
     descricao = models.TextField()
+    titulo_informativo = models.CharField(max_length=100, blank=True, null=True)
     informativo = models.TextField(null=True)
     preco = models.DecimalField(max_digits=10, decimal_places=2)
     foto = models.ImageField(upload_to="image/")
+    foto_secundaria = models.ImageField(upload_to="image/", blank=True, null=True)
+    modo_de_uso = models.TextField(null=True, blank=True)
+    beneficios = models.TextField(null=True, blank=True)
     destaques = models.BooleanField(default=False)
     slug = models.SlugField(unique=True, blank=True)
 
